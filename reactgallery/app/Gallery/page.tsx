9050cea9c479b99946dd647dfa5963ae37b516b0
Kartikey <codekartikey@gmail.com>
2023-07-10 22:23:50 +0530

    add homepage + gallery route

---
+++ reactgallery/app/Gallery/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { useState } from "react";
+import { motion } from "framer-motion";
+export default function Gallery(){
+    return(
+        <div className="bg-[#CBFFA9] min-h-screen">
+
+        </div>
+    )
+}
